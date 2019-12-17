bc0eae5f67f7e358541c77433fed7df6fbf0281a
Jasper Van der Jeugt <jasper@fugue.co>
2019-12-17 15:12:53 +0100

    Bump

--- main.tf
+++ main.tf
@@ -16,13 +16,6 @@ resource "aws_security_group" "good" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
-  egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks     = ["0.0.0.0/0"]
-  }
 }
 
 resource "aws_security_group" "bad" {
