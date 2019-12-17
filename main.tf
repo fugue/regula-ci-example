be8cc55deff57c8393d843967cf03c2831f7b508
Jasper Van der Jeugt <jasper@fugue.co>
2019-12-17 15:55:00 +0100

    Use ebs volumes

--- main.tf
+++ main.tf
@@ -1,39 +1,20 @@
 provider "aws" {
-  region = "eu-west-1"
+  region = "eu-west-2"
 }
 
-resource "aws_vpc" "example" {
-  cidr_block = "10.0.0.0/16"
+resource "aws_ebs_volume" "bad" {
+  availability_zone = "us-west-2a"
+  size              = 8
 }
 
-resource "aws_security_group" "good" {
-  name   = "good"
-  vpc_id = "aws_vpc.example.id"
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+resource "aws_ebs_volume" "good" {
+  availability_zone = "us-west-2a"
+  size              = 8
+  encrypted         = true
 }
 
-resource "aws_security_group" "bad" {
-  name   = "bad"
-  vpc_id = "aws_vpc.example.id"
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["77.56.34.72/32"]
-  }
-
-  egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks     = ["0.0.0.0/0"]
-  }
+resource "aws_ebs_volume" "large" {
+  availability_zone = "us-west-2a"
+  size              = 32
+  encrypted         = true
 }
-
