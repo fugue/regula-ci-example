cdc8bde830f0acdcfd298f02db859954ddf6c083
Jasper Van der Jeugt <jasper@fugue.co>
2019-12-17 15:00:58 +0100

    Try credentials

---
+++ main.tf
@@ -0,0 +1,45 @@
+provider "aws" {
+  region = "eu-west-1"
+}
+
+resource "aws_vpc" "example" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_security_group" "good" {
+  name   = "good"
+  vpc_id = "aws_vpc.example.id"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "bad" {
+  name   = "bad"
+  vpc_id = "aws_vpc.example.id"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["77.56.34.72/32"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+}
